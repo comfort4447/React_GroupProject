be494f07bee13e13796875c2d6bc98fc54312df6
Efosa Oviawe <josephoviawe4@gmail.com>
2022-02-14 14:08:44 +0100

    project structure update

---
+++ src/component/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Service from "./Service"
+import Footer from "./Footer"
+
+function App() {
+  return (
+    <div>
+
+      <Service />
+
+      <Footer />
+      
+    </div>
+  )
+}
+
+export default App;
