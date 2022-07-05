64ab9b2ecb067accb8ae160370116b8557f6cec9
Oladiran01 <victor.oladiran@elizadeuniversity.edu.ng>
2022-07-05 03:40:18 +0100

    full

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -2,20 +2,23 @@
 
 export default function Navbar() {
     return(
-            <div className="bkground-img">
-            <nav>
-                
-                    <img src="../images/interjet.png" alt="" className="nav-img" />
-                <div className="nav-details">
-                    <ul>
-                        <li><a href="/#">Home</a></li>
-                        <li><a href="/#">Services</a></li>
-                        <li><a href="/#">About</a></li>
-                        <button className="nav-start">Get Started</button>
-                    </ul>
-                </div>
-            </nav>
-            </div>
             
+        <div>
+        <div className="bkground-img">
+        <nav>
+
+                <img src="../images/interjet.png" alt="" className="nav-img" />
+            <div className="nav-details">
+                <ul>
+                    <li><a href="/#">Home</a></li>
+                    <li><a href="/#">Services</a></li>
+                    <li><a href="/#">About</a></li>
+                    <button className="nav-start">Get Started</button>
+                </ul>
+            </div>
+        </nav>
+        </div>
+
+    </div>
     )
 }
